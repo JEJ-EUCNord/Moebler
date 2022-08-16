390e4647dc9973c42bcb5fafce720e2fe27a2c9c
Jens Jensen <jej@eucnord.dk>
2022-08-16 09:30:17 +0200

    Add project files.

---
+++ Moebler/Program.cs
@@ -0,0 +1,20 @@
+﻿using Moebler;
+
+// Lav et nyt møbel
+Moebel mitMoebel = new Moebel();
+mitMoebel.Pris = 120.50;
+mitMoebel.Fabrikant = "Træfabrikken";
+
+// Lav et nyt TV
+TV mitTV= new TV();
+mitTV.Pris = 3399;
+mitTV.Fabrikant = "LG";
+mitTV.Stoerrelse = 66;
+
+// foretag testudskrivning
+Console.WriteLine(mitMoebel.Fabrikant);
+Console.WriteLine(mitMoebel.Pris);
+Console.WriteLine();
+Console.WriteLine(mitTV.Fabrikant);
+Console.WriteLine(mitTV.Stoerrelse);
+Console.WriteLine(mitTV.Pris);  
